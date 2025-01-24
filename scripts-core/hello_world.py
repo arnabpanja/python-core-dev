c758a5e3877ea987bbc915e4126246a0108c764c
arnabpanja <arnab.panja@gmail.com>
2025-01-24 19:03:47 +0530

    Changes to Hello World and some useful commands

--- scripts-core/hello_world.py
+++ scripts-core/hello_world.py
@@ -1,4 +1,5 @@
 def print_my_text(test_str):
-    print(test_str)
+    print('Hello ! ' + test_str)
 
-print_my_text('Hello World')
+your_name = input("Whats your name ? \n")
+print_my_text(your_name)
