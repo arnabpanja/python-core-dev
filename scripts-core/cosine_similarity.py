104e4c2dfaeb6641cac0e06318408bb72df6cec3
ArnabP <arnab.panja@gmail.com>
2025-03-23 20:41:13 +0530

    new changes

--- scripts-core/cosine_similarity.py
+++ scripts-core/cosine_similarity.py
@@ -2,6 +2,6 @@
 import numpy as np
 
 def compute_cosine_similarity(u: np.ndarray, v: np.ndarray) -> float:
-    """ compute cosine similarity between two vectors"""
+    """ compute cosine similarity between two vectors ... """
     return(u @ v) / (np.linalg.norm(u) * np.linalg.norm(v))
 
